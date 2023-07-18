f7f9e3afe8ed80f2c76a92034895c7648fc66e80
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-07-18 15:50:03 +0100

    Added a space after value in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 == "bootstrap" ]]; then
-    go run . -seed 1234 -port 61960 -duration $2&
+    go run . -seed 1234 -port 61960 -duration $2 &
 else
     go run . -peer /ip4/0.0.0.0/tcp/61960/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 -duration $1 &
 fi
