6fb460ba3a68f238e6b7e1904c275643da908804
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-07-19 13:15:04 +0100

    Updated for loop syntax.

--- run.sh
+++ run.sh
@@ -30,29 +30,29 @@ cd libp2p-das-datahop
 echo "Running builders"
 # Run builders
 for ((i=1; i<=$builder_count; i++))
-    do
-        echo "Running builder $i"
-        go run . -debug -seed 1234 -port 61960 -nodeType builder -duration $duration &
-        sleep 0.5
-    done
+do
+    echo "Running builder $i"
+    go run . -debug -seed 1234 -port 61960 -nodeType builder -duration $duration &
+    sleep 0.5
+done
 
 echo "Running validators"
 # Run validators
 for ((i=1; i<=$validator_count; i++))
-    do
-        echo "Running validator $i"
-        go run . -debug -duration $duration -nodeType validator -peer /ip4/127.0.0.1/tcp/61960/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 &
-        sleep 0.5
-    done
+do
+    echo "Running validator $i"
+    go run . -debug -duration $duration -nodeType validator -peer /ip4/127.0.0.1/tcp/61960/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 &
+    sleep 0.5
+done
 
 echo "Running non-validators"
 # Run non validators
 for ((i=1; i<=$non_validator_count; i++))
-    do
-        echo "Running non validator $i"
-        go run . -debug -duration $duration -nodeType nonvalidator -peer /ip4/127.0.0.1/tcp/61960/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 &
-        sleep 0.5
-    done
+do
+    echo "Running non validator $i"
+    go run . -debug -duration $duration -nodeType nonvalidator -peer /ip4/127.0.0.1/tcp/61960/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 &
+    sleep 0.5
+done
 
 if [!-d $result_dir]; then
     mkdir -p $result_dir;
