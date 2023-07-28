d5994fc1bfea7c806defd9abb52ce7107320e117
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-07-28 17:18:48 +0100

    Updated launch scripts - minor bugfixes in run.sh

--- experiment_launcher.py
+++ experiment_launcher.py
@@ -2,7 +2,29 @@ import logging
 import enoslib as en
 import os
 import datetime
+import subprocess
+import time
+#Upload launch script to site frontend
+def execute_ssh_command(launch_script, login, site):
+    # SSH command in the format 'ssh <username>@<hostname> "<command>"'
+    ssh_command = f'scp {launch_script} {login}@access.grid5000.fr:{site}'
 
+    try:
+        # Execute the SSH command
+        result = subprocess.run(ssh_command, shell=True, capture_output=True, text=True)
+        print("Script send to frontend")
+        # Check if the command was successful
+        if result.returncode == 0:
+            # Print the output
+            print(result.stdout)
+        else:
+            # Print the error message
+            print(result.stderr)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while executing SSH command: {e}")
+
+#Get timestamp after end of experiment
 def add_time(original_time, hours=0, minutes=0, seconds=0):
     time_delta = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
     new_time = original_time + time_delta
@@ -12,60 +34,130 @@ def convert_seconds_to_time(seconds):
     hours, remainder = divmod(seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
     return hours, minutes, seconds
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
-en.init_logging(level=logging.INFO)
-en.check()
+def seconds_to_hh_mm_ss(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
-login = "kpeeroo"
-nb_node = 101
-nb_builder = 1
-nb_validator = 50
-duration_secs = 30
-nb_non_validator = nb_node - nb_validator - nb_builder
-experiment_name = f"{nb_builder}_builders_{nb_validator}_validators_{nb_non_validator}_nonvalidators_{duration_secs}_secs"
+#Experiment node partition between Grid5000 machine
+def node_partition(nb_cluster_machine, nb_builder, nb_validator, nb_regular):
+    partition = [[0, 0, 0] for i in range(nb_cluster_machine)]
 
-nb_node_per_cpu = nb_node//10
+    index = 0
+    while nb_builder > 0 or nb_validator > 0 or nb_regular > 0:
+        if index == len(partition):
+            index  = 0
+        if nb_builder > 0:
+            partition[index][0] += 1
+            nb_builder -= 1
+        elif nb_validator > 0:
+            partition[index][1] += 1
+            nb_validator -= 1            
+        elif nb_regular > 0:
+            partition[index][2] += 1
+            nb_regular -= 1      
+        index += 1
+    return partition
+ 
+def main():
 
-network = en.G5kNetworkConf(type="prod", roles=["experiment_network"], site="grenoble")
+    #========== Parameters ==========
+    #Grid5000 parameters
+    login = "kpeeroo" #Grid5000 login
+    site = "nancy" #Grid5000 Site See: https://www.grid5000.fr/w/Status and https://www.grid5000.fr/w/Hardware
+    cluster = "gros" #Gride5000 Cluster name See: https://www.grid5000.fr/w/Status and https://www.grid5000.fr/w/Hardware
+    job_name = "PANDAS_libp2p"
 
-conf = (
-    en.G5kConf.from_settings(job_name="Louvain-job-1", walltime="00:02:00")
-    .add_network_conf(network)
-    .add_machine(roles=["first"], cluster="dahu", nodes=10, primary_network=network)
-    .finalize()
-)
+    #Node launch script path
+    dir_path = os.path.dirname(os.path.realpath(__file__)) #Get current directory path
+    launch_script = dir_path +"/" + "run.sh"
 
-# This will validate the configuration, but not reserve resources yet
-provider = en.G5k(conf)
-roles, networks = provider.init(force_deploy=True)
-roles = en.sync_info(roles, networks)
+    #Experiment parameters
+    nb_cluster_machine = 10         #Number of machine booked on the cluster
+    nb_experiment_node = 160        #Number of nodes running for the experiment
+    nb_builder = 1
+    nb_validator = 40
+    nb_regular = nb_experiment_node - nb_builder - nb_validator
+    exp_duration = 60               #In seconds
+    experiment_name = f"PANDAS_libp2p_{nb_builder}b_{nb_validator}v_{nb_regular}r_{exp_duration}sec_"
+    current_datetime = datetime.datetime.now()
+    experiment_name += current_datetime.strftime("%Y-%m-%d-%H:%M:%S") 
+    
+    #Network parameters 
+    delay = "10%"
+    rate = "1gbit"
+    loss = "0%"
+    symmetric=True
 
-#Network emulation
-netem = en.NetemHTB()
-(
-    netem.add_constraints(
-        src=roles["first"],
-        dest=roles["first"],
-        delay="70ms",
-        rate="1gbit",
-        symmetric=True,)
-)
+    #========== Experiment nodes partition on cluster machines ==========
+    partition = node_partition(nb_cluster_machine, nb_builder, nb_validator, nb_regular)
 
-netem.deploy()
-netem.validate()
 
-with en.actions(roles=roles["first"], on_error_continue=True, background=True) as p:
-    p.shell(f"/home/{login}/run.sh {duration_secs} {experiment_name} {nb_builder} {nb_validator} {nb_non_validator}")
+    #========== Create and validate Grid5000 and network emulation configurations ==========
+    #Log to Grid5000 and check connection
+    en.init_logging(level=logging.INFO)
+    en.check()
+    network = en.G5kNetworkConf(type="prod", roles=["experiment_network"], site=site)
+    
+    Job_walltime = seconds_to_hh_mm_ss(exp_duration + 120)
+    
+    conf = (
+        en.G5kConf.from_settings(job_name=job_name, walltime=Job_walltime)
+        .add_network_conf(network)
+        .add_machine(roles=["experiment"], cluster=cluster, nodes=nb_cluster_machine, primary_network=network) # Add experiment nodes
+        .finalize()
+    )
 
-x = datetime.datetime.now()
-h,m,s = convert_seconds_to_time(duration_secs)
-finish_time = add_time(x,h,m,s)
-print("Begin at: ",x)
-print("Expected to finish at: ", finish_time)
+    #Validate Grid5000 configuration
+    start = datetime.datetime.now() #Timestamp grid5000 job start
+    provider = en.G5k(conf)
+    roles, networks = provider.init(force_deploy=True)
+    roles = en.sync_info(roles, networks)
 
-print(f"Results will be available at: /results/{experiment_name}_{finish_time}")
 
-# Release all Grid'5000 resources
-#netem.destroy()
-#provider.destroy()
+    #========== Grid5000 network emulation configuration ==========
+    #network parameters
+    netem = en.NetemHTB()
+    (
+        netem.add_constraints(
+            src=roles["experiment"],
+            dest=roles["experiment"],
+            delay=delay,
+            rate=rate,
+            loss=loss,
+            symmetric=symmetric,
+        )
+    )
+    
+    #Deploy network emulation
+    netem.deploy()
+    netem.validate()
+
+
+    #========== Deploy Experiment ==========
+    #Send launch script to Grid5000 site frontend
+    execute_ssh_command(launch_script, login, site)
+    i = 0
+    for x in roles["experiment"]:
+        with en.actions(roles=x, on_error_continue=True, background=True) as p:
+            builder, validator, regular = partition[i]
+            p.shell(f"/home/{login}/run.sh {exp_duration} {experiment_name} {builder} {validator} {regular} {login}")
+            i += 1
+
+    start = datetime.datetime.now() #Timestamp grid5000 job start
+
+    #========== Wait job and and release grid5000 ressources ==========
+    #Print experiment duration
+    h,m,s = convert_seconds_to_time(exp_duration)
+    print("Begin at: ",start)
+    print("Expected to finish at: ",add_time(start,h,m,s + 10))
+    time.sleep(exp_duration + 20)
+
+    #Release all Grid'5000 resources
+    netem.destroy()
+    provider.destroy()
+
+if __name__ == "__main__":
+    main()
--- run.sh
+++ run.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 # Script parameters: 
-# $1 = duration
-# $2 = experiment_name
-# $3 = builder count
-# $4 = validator count
-# $5 = non validator count
+# $1=duration
+# $2=experiment_name
+# $3=builder count
+# $4=validator count
+# $5=non validator count
 
-duration=$1
+exp_duration=$1
 experiment_name=$2
 builder_count=$3
 validator_count=$4
 non_validator_count=$5
+login=$6
 
-result_dir="/home/kpeeroo/results"
+result_dir="/home/${login}/results"
+finish_time=$(date +%d-%m-%y-%H-%M)
+result_dir="${result_dir}/${experiment_name}_${finish_time}"
+
+if [ ! -d "$result_dir" ]; then
+    mkdir -p "$result_dir"
+fi
 
 #Install go
 echo "Installing go"
@@ -29,7 +36,7 @@ cd libp2p-das-datahop
 
 echo "Running builders"
 # Run builders
-for ((i=1; i<=$builder_count- 1; i++))
+for ((i=1; i<=$builder_count; i++))
 do
     echo "Running builder $i"
     go run . -debug -seed 1234 -port 61960 -nodeType builder -duration $duration &
@@ -38,7 +45,7 @@ done
 
 echo "Running validators"
 # Run validators
-for ((i=1; i<=$validator_count - 1; i++))
+for ((i=0; i<=$validator_count - 1; i++))
 do
     echo "Running validator $i"
     go run . -debug -duration $duration -nodeType validator -peer /ip4/127.0.0.1/tcp/61960/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 &
@@ -47,14 +54,14 @@ done
 
 if [ $(($non_validator_count)) -eq 0 ]
 then
-    go run . -debug -seed 1234 -port 61960 -nodeType builder -duration $duration
+    go run . -debug -seed 1234 -port 61960 -nodeType validator -duration $duration
 else
-    go run . -debug -seed 1234 -port 61960 -nodeType builder -duration $duration &
+    go run . -debug -seed 1234 -port 61960 -nodeType validator -duration $duration &
 fi
 
 echo "Running non-validators"
 # Run non validators
-for ((i=1; i<=$non_validator_count - 1; i++))
+for ((i=0; i<=$non_validator_count - 1; i++))
 do
     echo "Running non validator $i"
     go run . -debug -duration $duration -nodeType nonvalidator -peer /ip4/127.0.0.1/tcp/61960/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 &
@@ -62,15 +69,8 @@ do
 done
 
 if [ $(($non_validator_count)) -ne 0 ]; then
-    go run . -debug -seed 1234 -port 61960 -nodeType builder -duration $duration
+    go run . -debug -seed 1234 -port 61960 -nodeType nonvalidators -duration $duration
 fi;
-
-
-if [ ! -d $result_dir ]; then
-    mkdir -p $result_dir;
-fi;
-
-$finish_time = $(date +%d-%m-%y-%H-%M)
 
 mkdir "${result_dir}/${experiment_name}_${finish_time}}"
 cp *.csv "${result_dir}/${experiment_name}_${finish_time}}"
